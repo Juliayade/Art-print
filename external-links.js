51b73e3006f8a0fcc530fa26127f3250e4bee768
Juliayade <julialapuyade@gmail.com>
2026-01-01 22:40:53 +0100

    Update external-links.js

--- external-links.js
+++ external-links.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-   alert("JS OK");
   document.querySelectorAll("a[href]").forEach(link => {
     if (link.hostname && link.hostname !== location.hostname) {
       link.target = "_blank";
       link.rel = "noopener noreferrer";
     }
-   
   });
 });
